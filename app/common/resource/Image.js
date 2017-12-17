9093c3f4316a9e6b678672ae44fa5e385f6f7140
gaojia <gaojia@sinooceangroup.com>
2017-12-17 22:29:11 +0800

    init

---
+++ app/common/resource/Image.js
@@ -0,0 +1,35 @@
+/**
+ * 描述：
+ * 2017/10/28
+ * 作者：高佳
+ */
+
+export default Images = {
+	selected: {
+		source: require('../../images/selected.png')
+	},
+	select_drop: {
+		source: require('../../images/drop_icon.png')
+	},
+	select_up: {
+		source: require('../../images/up_icon.png')
+	},
+	empty_page: {
+		source: require('../../images/empty_page.png'),
+	},
+	default_cover: {
+		source: require('../../images/default_cover.png'),
+	},
+	ship_icon: {
+		source: require('../../images/ship_icon.png'),
+	},
+	ship_plate: {
+		source: require('../../images/ship_plate.png'),
+	},
+	telephone: {
+		source: require('../../images/telephone.png'),
+	},
+	location: {
+		source: require('../../images/location.png'),
+	},
+};
